953d3772fddd517a68364e95a8228308b06bf742
Merlin Fejzuli <m.a.fejzuli@gmail.com>
2020-11-06 02:10:37 +0100

    refactored data parsing added prof selector

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,22 +9,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
+    private static $SEMESTERS = [
+        [ 'value' => '#SPLUS1428E2', 'name' => 'BI 1' ],
+        [ 'value' => '#SPLUSF85D4A', 'name' => 'BWI 1' ]
+    ];
+    private static $GROUP_NUMBERS = [ '', 1, 2, 3, 4, 5, 6, 7, 8 ];
+    private static $GROUP_LETTERS = [ '', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M' ];
+    private static $ORGANIZERS = [ '', 'Berrendorf', 'Priesnitz' ];
+
     /**
      * @Route("/", name="home")
      */
     public function getHome(): Response
     {
-        $semesters = [
-            [ 'value' => '#SPLUS1428E2', 'name' => 'BI 1' ],
-            [ 'value' => '#SPLUSF85D4A', 'name' => 'BWI 1' ]
-        ];
-        $groupNumbers = [ '', 1, 2, 3, 4, 5, 6, 7, 8 ];
-        $groupLetters = [ '', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M' ];
-
         return $this->render('home/home.html.twig', [
-            'semesters' => $semesters,
-            'groupNumbers' => $groupNumbers,
-            'groupLetters' => $groupLetters
+            'semesters' => self::$SEMESTERS,
+            'groupNumbers' => self::$GROUP_NUMBERS,
+            'groupLetters' => self::$GROUP_LETTERS,
+            'organizers' => self::$ORGANIZERS
         ]);
     }
 }
